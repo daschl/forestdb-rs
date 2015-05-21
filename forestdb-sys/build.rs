665b5a1c5b2c9f76a1616be950307396296d9fb7
Michael Nitschinger <michael@nitschinger.at>
2015-05-21 21:01:12 +0200

    Initial Commit (not working)

---
+++ forestdb-sys/build.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs::{self, File};
+use std::io::ErrorKind;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn main() {
+
+	let _ = fs::create_dir("forestdb/build");
+
+	 run(Command::new("cmake").arg("../").current_dir("forestdb/build"), "cmake");
+	 run(Command::new("make").arg("all").current_dir("forestdb/build"), "make");
+}
+
+fn run(cmd: &mut Command, program: &str) {
+    println!("running: {:?}", cmd);
+    let status = match cmd.status() {
+        Ok(status) => status,
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            fail(&format!("failed to execute command: {}\nis `{}` not installed?",
+                          e, program));
+        }
+        Err(e) => fail(&format!("failed to execute command: {}", e)),
+    };
+    if !status.success() {
+        fail(&format!("command did not execute successfully, got: {}", status));
+    }
+}
+
+fn fail(s: &str) -> ! {
+    panic!("\n{}\n\nbuild script failed, must exit now", s)
+}
---
+++ forestdb-sys/lib.rs
@@ -0,0 +1,12 @@
+extern crate libc;
+
+extern {
+	pub fn fdb_init();
+}
+
+#[test]
+fn foo() {
+	unsafe {
+		fdb_init();
+	}
+}
