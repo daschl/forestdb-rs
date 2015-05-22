ca4ee451790bef8e5aa9a22fab648b2dd28c44a8
Michael Nitschinger <michael@nitschinger.at>
2015-05-22 08:09:34 +0200

    it seems to compile

--- forestdb-sys/build.rs
+++ forestdb-sys/build.rs
@@ -15,7 +15,8 @@ fn main() {
 	 run(Command::new("make").arg("all").current_dir("forestdb/build"), "make");
 	 run(Command::new("cp").arg("-r").arg("forestdb/build").arg(dst), "mv");
 
-	 println!("cargo:rustc-link-search=dylib={}", &env::var("OUT_DIR").unwrap());
+	 println!("cargo:rustc-link-lib=dylib=forestdb");
+	 println!("cargo:rustc-link-search=all={}", &env::var("OUT_DIR").unwrap());
 }
 
 fn run(cmd: &mut Command, program: &str) {
--- forestdb-sys/lib.rs
+++ forestdb-sys/lib.rs
@@ -1,12 +1,12 @@
 extern crate libc;
 
 extern {
-	//pub fn fdb_init();
+	pub fn fdb_init();
 }
 
 #[test]
 fn foo() {
 	unsafe {
-		//fdb_init();
+		fdb_init();
 	}
 }
